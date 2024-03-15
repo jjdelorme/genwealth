a552ad117f9e2ce3cb24e49ff197a787c288b6d0
Jason De Lorme <jasondel@google.com>
2024-03-15 20:10:22 +0000

    Styling for use filters.

--- ui/src/app/prospects/prospects.component.ts
+++ ui/src/app/prospects/prospects.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { ProspectResultsComponent } from '../prospect-results/prospect-results.component';
 
@@ -21,6 +22,7 @@ import { ProspectResultsComponent } from '../prospect-results/prospect-results.c
     MatInputModule,
     MatCardModule,
     MatSliderModule,
+    MatSlideToggleModule,
     ProspectResultsComponent,
   ],
   templateUrl: './prospects.component.html',
@@ -30,11 +32,33 @@ export class ProspectsComponent {
   constructor(private genWealthClient: GenWealthServiceClient) {}
 
   prospectSearch: string = '';
+  useFilters: boolean = true;
   minAge: number = 21;
   maxAge: number = 65;
   riskProfile: number = 1;
 
   prospects?: Observable<QueryResponse<Prospect>> = undefined;
+
+  useFiltersChange(change: MatSlideToggleChange) {
+    this.useFilters = change.checked;
+  }
+
+  formatRiskLabel(value: number): string {
+    switch (value) {
+      case 1:
+        return 'Low';
+        break;
+      case 2:
+        return 'Medium';
+        break;
+      case 3:
+        return 'High';
+        break;
+      default:
+        return '';
+        break;
+    }
+  }
 
   findProspects() {
     this.prospects = 
