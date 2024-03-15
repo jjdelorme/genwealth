d0845454c8798d33376e9e1021cfb74851caf4a7
Jason De Lorme <jasondel@google.com>
2024-03-15 18:28:42 +0000

    Reformatting

--- ui/src/app/prospect-results/prospect-results.component.ts
+++ ui/src/app/prospect-results/prospect-results.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, map, tap } from 'rxjs';
-
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
 
 import { Prospect } from '../services/genwealth-api';
 import { TextToHtmlPipe } from '../services/text-to-html.pipe';
@@ -33,13 +32,12 @@ import { TextToHtmlPipe } from '../services/text-to-html.pipe';
 export class ProspectResultsComponent {
   @Input()
   set prospects(value: Observable<Prospect[]>) {
-    console.log('prospects set');
-    value.subscribe({ next: data => { this.dataSource.data = data; console.log('data', data); }});
+    value.subscribe({ next: data => this.dataSource.data = data });
   }
 
   columnsToDisplay: string[] = ['id', 'firstName','lastName','email','age','riskProfile'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement?: Prospect | null;  // = undefined
+  expandedElement?: Prospect | null;  
   dataSource = new MatTableDataSource<Prospect>();
   query?: string = undefined;
 }
