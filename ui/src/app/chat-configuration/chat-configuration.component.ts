6e484ad240f7502e89cf1ce5420984d9946904fb
Jason De Lorme <jasondel@google.com>
2024-03-16 01:52:00 +0000

    Update styles

--- ui/src/app/chat-configuration/chat-configuration.component.ts
+++ ui/src/app/chat-configuration/chat-configuration.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ChatRequest } from '../services/genwealth-api';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-chat-configuration',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,    
+    FormsModule,
+  ],
   templateUrl: './chat-configuration.component.html',
   styleUrl: './chat-configuration.component.scss'
 })
