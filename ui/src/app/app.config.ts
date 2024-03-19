3a40c29b9967608c2ba8fb2643f6b169ae232098
Jason De Lorme <delormej@gmail.com>
2024-03-19 13:53:34 -0400

    Config to /api

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideAnimationsAsync(),
     {
       provide: BASE_URL,
-      useValue: 'https://genwealth-3tl63eyydq-uc.a.run.app/api'
+      useValue: '/api'
     },
     importProvidersFrom(HttpClientModule),
   ]
