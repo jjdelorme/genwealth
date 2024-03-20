a99b939c4140fadf26da20d095ad080353af8c3e
Jason De Lorme <delormej@gmail.com>
2024-03-20 12:14:51 -0400

    Add default values / reset.

--- ui/src/app/chat/configuration/chat-configuration.component.ts
+++ ui/src/app/chat/configuration/chat-configuration.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
@@ -21,6 +22,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatTooltipModule,    
     FormsModule,
+    MatButtonModule
   ],
   templateUrl: './chat-configuration.component.html',
   styleUrl: './chat-configuration.component.scss'
@@ -29,4 +31,29 @@ export class ChatConfigurationComponent {
   @Input()
   chatRequest?: ChatRequest;
 
+  defaultRequest: ChatRequest = {
+    prompt: '',
+    advanced: true,
+    useHistory: true,
+    llmRole: 'You are a financial chatbot named Penny',
+    mission: 'Your mission is to assist your clients by providing financial education, account details, and basic information related to budgeting, saving, and different types of investments',
+    outputInstructions: 'Begin your response with a professional greeting. Greet me by name if you know it. End your response with a signature that includes your name and GenWealth company affiliation.',
+    responseRestrictions: 'You are not a licensed financial advisor, so your must never provide financial advice under any circumstance. Always start your response by warning that you are not authorized to provide financial advice. If you are asked for financial advice, politely decline to answer, and offer to help with financial education, account information, and basic information related to budgeting, saving, and types of investments.',
+    disclaimer: 'LEGAL DISCLAIMER: All output from this chatbot is provided for informational purposes only. It is not intended to provide (and should not be relied on for) tax, investment, legal, accounting or financial advice. You should consult your own licensed tax, investment, legal and accounting advisors before engaging in any transaction.',
+  }
+
+  useDefault() {
+    this.chatRequest = {
+      ...this.defaultRequest, 
+      prompt: this.chatRequest?.prompt ?? '',
+      userId: this.chatRequest?.userId,
+    };
+  }
+
+  reset() {
+    const request = new ChatRequest(this.chatRequest?.prompt ?? '');
+    request.userId = this.chatRequest?.userId;
+
+    this.chatRequest = request;
+  }
 }
