369c91b2a015680fa5f95fc5dbc998016205f001
Jason De Lorme <delormej@gmail.com>
2024-03-21 08:45:36 -0400

    Generating metdata

--- api/prospectus.ts
+++ api/prospectus.ts
@@ -1,42 +1,55 @@
 import { Storage } from '@google-cloud/storage';
 import { SearchServiceClient } from '@google-cloud/discoveryengine';
+import { v4 as uuidv4 } from 'uuid';
 
 /** 
  */
 export class Prospectus {
-    private readonly projectId: string;
-    private readonly dataStoreId: string;
+    private readonly storageClient: Storage;
+    private readonly searchClient: SearchServiceClient;
+    private readonly searchServingConfig: string;
 
     constructor() {
-        this.projectId = process.env['PROJECT_ID'];
-        if (!this.projectId) {
+        const projectId = process.env['PROJECT_ID'];
+        if (!projectId) {
             throw new Error('PROJECT_ID environment variable not set');
         }
-        this.dataStoreId = process.env['STORE_ID'];
+        const dataStoreId = process.env['DATASTORE_ID'];
+        if (!dataStoreId) {
+            throw new Error('DATASTORE_ID environment variable not set');
+        }
+
+        this.storageClient = new Storage({projectId: projectId});
+
+        this.searchClient = new SearchServiceClient();
+        this.searchServingConfig = this.searchClient.projectLocationCollectionDataStoreServingConfigPath(
+            projectId,
+            "global",
+            "default_collection",
+            dataStoreId,
+            "default_search"
+          );
     }
 
     async upload(buffer: Buffer, filename: string, ticker?: string) {
         const bucketName = process.env['PROSPECTUS_BUCKET'];
         if (!bucketName)
             throw new Error("PROSPECTUS_BUCKET environment variable not set");
-
-        const storage = new Storage({projectId: this.projectId});
-        const blob = storage.bucket(bucketName).file(filename);
+       
+        const blob = this.storageClient.bucket(bucketName).file(filename);
 
         await blob.save(buffer);
+
+        const metadata = this.getMetadata(blob.cloudStorageURI.toString(), ticker);
+        const metadataBlob = this.storageClient.bucket(bucketName).file(`${ticker}.jsonl`);
+        await metadataBlob.save(JSON.stringify(metadata));
+
+        console.log(`Uploaded ${filename} to ${bucketName}`);
+
+        // TODO: index the file
     }
 
     async search(query: string, ticker?: string) {
-        const client = new SearchServiceClient();
-
-        const name = client.projectLocationCollectionDataStoreServingConfigPath(
-            this.projectId,
-            "global",
-            "default_collection",
-            this.dataStoreId,
-            "default_search"
-          );
-
           const request = {
             pageSize: 5,
             query: query,
@@ -54,11 +67,11 @@ export class Prospectus {
                 }
             },
             filter: `ticker: ANY(\"${ticker}\")`,
-            servingConfig: name,
+            servingConfig: this.searchServingConfig,
           };
                
           // Perform search request
-          const response = await client.search(request, {
+          const response = await this.searchClient.search(request, {
             autoPaginate: false,
           });
           const results = response;
@@ -67,5 +80,13 @@ export class Prospectus {
             console.log(result);
           }        
 
+    }
+
+    private getMetadata(gcsPath: string, ticker: string) {
+        return {
+            id: uuidv4(),
+            structData: { ticker: ticker },
+            content: { mimeType: "application/pdf", uri: gcsPath }
+        };
     }
 }
