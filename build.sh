df9b7537fdb40d73c58968ada68a0d799b4b1a46
Jason De Lorme <delormej@gmail.com>
2024-03-24 20:55:34 -0400

    Break apart build & deploy

---
+++ build.sh
@@ -0,0 +1,30 @@
+REGION=$(gcloud config get-value run/region)
+if [ -z "$REGION" ]; then
+  echo "REGION is not set. Please set the gcloud run/region."
+  exit 1
+fi
+
+#
+# Build & push the container
+#
+TAG_NAME=$(git describe --abbrev=0 --tags)
+IMAGE=$REGION-docker.pkg.dev/$PROJECT_ID/genwealth/genwealth:$TAG_NAME
+
+docker build -t $IMAGE .
+docker push $IMAGE
+
+#
+# Step 3: Deploy to Cloud Run
+#
+gcloud beta run deploy genwealth \
+    --image=$IMAGE \  
+    --execution-environment=gen2 \
+    --cpu-boost \
+    --network=$VPC_NETWORK \
+    --subnet=$VPC_SUBNET \
+    --vpc-egress=private-ranges-only \
+    --region=$REGION \
+    --project=$PROJECT_ID \
+    --allow-unauthenticated \
+    --set-env-vars=PGHOST=$PGHOST,PGPORT=$PGPORT,PGDATABASE=$PGDATABASE,PGUSER=$PGUSER,PGPASSWORD=$PGPASSWORD,DATASTORE_ID=$DATASTORE_ID,PROSPECTUS_BUCKET=$PROSPECTUS_BUCKET
+
--- deploy.sh
+++ deploy.sh
@@ -23,13 +23,12 @@ DATASTORE_ID=xxxx # Datastore ID used by Vertex S&C
 PROSPECTUS_BUCKET=xxxx # GCS Bucket for storing pro
 
 PROJECT_ID=$(gcloud config get-value project)
-REGION=$(gcloud config get-value run/region)
-
 if [ -z "$PROJECT_ID" ]; then
   echo "PROJECT_ID is not set."
   exit 1
 fi
 
+REGION=$(gcloud config get-value run/region)
 if [ -z "$REGION" ]; then
   echo "REGION is not set. Please set the gcloud run/region."
   exit 1
@@ -53,24 +52,4 @@ gcloud artifacts repositories create genwealth \
 #
 # Build & push the container
 #
-TAG_NAME=$(git describe --abbrev=0 --tags)
-IMAGE=$REGION-docker.pkg.dev/$PROJECT_ID/genwealth/genwealth:$TAG_NAME
-
-docker build -t $IMAGE .
-docker push $IMAGE
-
-#
-# Step 3: Deploy to Cloud Run
-#
-gcloud beta run deploy genwealth \
-    --image=$IMAGE \
-    --execution-environment=gen2 \
-    --cpu-boost \
-    --network=$VPC_NETWORK \
-    --subnet=$VPC_SUBNET \
-    --vpc-egress=private-ranges-only \
-    --region=$REGION \
-    --project=$PROJECT_ID \
-    --allow-unauthenticated \
-    --set-env-vars=PGHOST=$PGHOST,PGPORT=$PGPORT,PGDATABASE=$PGDATABASE,PGUSER=$PGUSER,PGPASSWORD=$PGPASSWORD,DATASTORE_ID=$DATASTORE_ID,PROSPECTUS_BUCKET=$PROSPECTUS_BUCKET
-
+source ./build.sh
